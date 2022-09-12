a7f8b637ddecf25741441c1a3961ea23e51b98b2
Eric <ericismag@outlook.com>
2022-09-12 17:58:52 +0300

    Fix 1st program

--- 1st_Program/Program.cs
+++ 1st_Program/Program.cs
@@ -4,20 +4,19 @@
 Console.WriteLine ("Введите пятизначное число");
 int Number = Convert.ToInt32(Console.ReadLine());
 
-if (Number / 10000 != 0) // проверили что число пятизначное
+if (Number / 10000 != 0 && Number / 10000 < 10) // проверили что число пятизначное
 {
-    int Number1 = Number;
-    if (Number / 10000 == Number % 10) // проверка равенства первой и последней цифры числа
-    {
-        Number %=  10000;
-        Number /= 10;
-    }
-    else 
-        Console.Write ($"Число {Number} не палиндром");
+   if (Number / 10000 == Number % 10)
+   {
+    Number /= 10;
+    Number %= 1000;
     if (Number / 100 == Number %10)
-        Console.Write ($"Число {Number1} является палиндромом");
+        Console.Write ("Число является палиндромом");
     else
-        Console.Write ($"Число {Number1} не является палиндромом");
+        Console.Write ("Число не палиндром");
+   }
+   else
+    Console.Write ("Число не палиндром");
 }
 else 
     Console.WriteLine ("Введено неправильное число");
