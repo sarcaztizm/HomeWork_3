e70a246b5e5f94543609caf4d2dfbb4d8cc4aa33
Eric <ericismag@outlook.com>
2022-09-12 17:25:39 +0300

    Third Programm

---
+++ 3rd_Program/Program.cs
@@ -0,0 +1,12 @@
+﻿// Программа принимает на вход число N
+// и выдает таблицу кубов чисел от 1 до N
+
+Console.WriteLine ("Введите число N");
+int N = Convert.ToInt32(Console.ReadLine());
+int count = 1;
+
+while (count <= N)
+{
+    Console.Write ($" {Math.Pow(count, 3)} ");
+    count++;
+}
